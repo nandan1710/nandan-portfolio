f2aba475fd2e987e34642f515ee4d618ade2aae4
nandan1710 <nandan.nejikar123@gmail.com>
2026-09-24 15:07:19 +0530

    Simplify getWordPressAccessToken function
    Refactor getWordPressAccessToken to simplify token retrieval.

--- src/lib/wordpress-auth.ts
+++ src/lib/wordpress-auth.ts
@@ -1,17 +1,7 @@
 import { cookies } from "next/headers";
 
-export async function getWordPressAccessToken() {
+export function getWordPressAccessToken() {
   const cookieStore = cookies();
 
-  const token = cookieStore.get(
-    "wordpress_access_token"
-  )?.value;
-
-  if (!token) {
-    throw new Error(
-      "WordPress is not connected. Please authorize first."
-    );
-  }
-
-  return token;
+  return cookieStore.get("wordpress_access_token")?.value;
 }
